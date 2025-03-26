5414a682d1b9f78e8bc02288110dddddee002829
osemeniha <semolga22@gmail.com>
2025-03-26 18:13:45 +0300

    task 184255

--- about-service/contacts/feedback.php
+++ about-service/contacts/feedback.php
@@ -1,14 +1,35 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Обратная связь");
-?><?$APPLICATION->IncludeComponent(
-	"bitrix:main.feedback",
-	"",
-	Array(
-		"EMAIL_TO" => "o.semeniha@yandex.ru",
-		"EVENT_MESSAGE_ID" => array(),
-		"OK_TEXT" => "Спасибо, ваше сообщение принято.",
-		"REQUIRED_FIELDS" => array("NAME","EMAIL","MESSAGE"),
-		"USE_CAPTCHA" => "Y"
-	)
-);?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+?>
+<div class="site-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 col-lg-8 mb-5">
+                <?$APPLICATION->IncludeComponent(
+                    "bitrix:main.feedback",
+                    "contacts.feedback",
+                    Array(
+                        "EMAIL_TO" => "o.semeniha@yandex.ru",
+                        "EVENT_MESSAGE_ID" => array("7"),
+                        "OK_TEXT" => "Спасибо, ваше сообщение принято.",
+                        "REQUIRED_FIELDS" => array("NAME","EMAIL","MESSAGE"),
+                        "USE_CAPTCHA" => "N"
+                    )
+                );?>
+            </div>
+            <div class="col-lg-4">
+                <?$APPLICATION->IncludeComponent(
+                    "bitrix:main.include",
+                    "",
+                    Array(
+                        "AREA_FILE_SHOW" => "file",
+                        "PATH" => "/include/feedback.php",
+                    )
+                );?>
+        </div>
+    </div>
+</div>
+
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
---
+++ local/templates/home/components/bitrix/main.feedback/contacts.feedback/template.php
@@ -0,0 +1,67 @@
+<?
+if(!defined("B_PROLOG_INCLUDED")||B_PROLOG_INCLUDED!==true)die();
+/**
+ * Bitrix vars
+ *
+ * @var array $arParams
+ * @var array $arResult
+ * @var CBitrixComponentTemplate $this
+ * @global CMain $APPLICATION
+ * @global CUser $USER
+ */
+?>
+<?if(!empty($arResult["ERROR_MESSAGE"]))
+{
+	foreach($arResult["ERROR_MESSAGE"] as $v)
+		ShowError($v);
+}
+if(!empty($arResult["OK_MESSAGE"]))
+{
+	?><div class="mf-ok-text"><?=$arResult["OK_MESSAGE"]?></div><?
+}
+?>
+
+<form class="p-5 bg-white border" action="<?=POST_FORM_ACTION_URI?>" method="POST">
+<?=bitrix_sessid_post()?>
+
+    <div class="row form-group">
+        <div class="col-md-12 mb-3 mb-md-0">
+            <label class="font-weight-bold" for="fullname"><?=GetMessage("MFT_NAME")?></label>
+            <input type="text" id="fullname" class="form-control" placeholder="<?=GetMessage("MFT_NAME")?>"
+                   value="<?=$arResult["AUTHOR_NAME"]?>">
+        </div>
+    </div>
+    <div class="row form-group">
+        <div class="col-md-12">
+            <label class="font-weight-bold" for="email"><?=GetMessage("MFT_EMAIL")?></label>
+            <input type="email" id="email" class="form-control" placeholder="<?=GetMessage("MFT_EMAIL")?>"
+                   value="<?=$arResult["AUTHOR_EMAIL"]?>" >
+        </div>
+    </div>
+
+    <div class="row form-group">
+        <div class="col-md-12">
+            <label class="font-weight-bold" for="message"><?=GetMessage("MFT_MESSAGE")?></label>
+            <textarea name="message" id="message" cols="30" rows="5" class="form-control"
+                      placeholder="<?=GetMessage("MFT_MESSAGE")?>"><?=($arResult["MESSAGE"] ?? '')?></textarea>
+        </div>
+    </div>
+
+    <?if($arParams["USE_CAPTCHA"] == "Y"):?>
+    <div class="row form-group">
+        <div class="col-md-12">
+            <div class="font-weight-bold"><?=GetMessage("MFT_CAPTCHA")?></div>
+            <input type="hidden" name="captcha_sid" value="<?=$arResult["capCode"]?>">
+            <img src="/bitrix/tools/captcha.php?captcha_sid=<?=$arResult["capCode"]?>" width="180" height="40" alt="CAPTCHA">
+            <div class="font-weight-bold"><?=GetMessage("MFT_CAPTCHA_CODE")?></div>
+            <input type="text" name="captcha_word" class="form-control" value="" >
+        </div>
+    </div>
+    <?endif;?>
+
+    <div class="row form-group">
+        <div class="col-md-12">
+            <input type="submit" value="<?=GetMessage("MFT_SUBMIT")?>" class="btn btn-primary  py-2 px-4 rounded-0">
+        </div>
+    </div>
+</form>
